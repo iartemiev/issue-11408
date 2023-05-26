6b12fe230c7c2c01395993b27c55d3599b73e19a
Ivan Artemiev <29709626+iartemiev@users.noreply.github.com>
2023-05-26 12:23:43 -0400

    Initial commit

--- App.js
+++ App.js
@@ -1,20 +1,67 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState, useCallback } from "react";
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View } from "react-native";
+import { NavigationContainer, useFocusEffect } from "@react-navigation/native";
+import { Amplify, API, graphqlOperation } from "aws-amplify";
+import { onCreateTodo } from "./src/graphql/subscriptions";
+import { createTodo } from "./src/graphql/mutations";
+import awsconfig from "./src/aws-exports";
+
+Amplify.configure(awsconfig);
+
+function LastEvent() {
+  const [lastEvent, setLastEvent] = useState({});
+
+  useFocusEffect(
+    useCallback(() => {
+      const subscription = API.graphql(
+        graphqlOperation(onCreateTodo)
+      ).subscribe({
+        next: async (event) => {
+          console.log("event received", event);
+          setLastEvent(event.value);
+        },
+        error: (error) => {
+          console.log("error in chats subscription useChatRoomUsers", {
+            error,
+          });
+        },
+      });
+
+      return () => subscription.unsubscribe();
+    }, [])
+  );
+
+  return (
+    <>
+      <Text>Last Received Event:</Text>
+      <Text>{JSON.stringify(lastEvent, null, 2)}</Text>
+    </>
+  );
+}
+
+async function newTodo() {
+  const newTodoInput = { input: { name: "Todo " + Date.now() } };
+  await API.graphql(graphqlOperation(createTodo, newTodoInput));
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <View style={styles.container}>
+        <Button onPress={newTodo} title="New Todo"></Button>
+        <LastEvent />
+        <StatusBar style="auto" />
+      </View>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "11408": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
